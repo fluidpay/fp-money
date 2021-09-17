7b463e6ea31b4fd5899f69cc5b64288a1c5b2d7d
bvoelker <bvoelker@fluidpay.com>
2021-09-17 16:47:32 -0500

    settings - updated eslint and vscode settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -14,8 +17,34 @@ module.exports = {
     'no-empty-pattern': 'off',
     'no-new': 'off',
 
-    // Vue ones
-    'vue/no-side-effects-in-computed-properties': 'off'
+    // Vue
+    "vue/no-v-model-argument": "off",
+    "vue/order-in-components": ["error", {
+      "order": [
+        "name",
+        ["components", "directives", "filters"],
+        ["props", "propsData"],
+        "data",
+        "computed",
+        "watch",
+        "methods"
+      ]
+    }],
+
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 5,
+        "allowFirstLine": true
+      },      
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }],
+
+    "vue/component-tags-order": ["error", {
+      "order": [ "script", "style", "template" ]
+    }]
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
