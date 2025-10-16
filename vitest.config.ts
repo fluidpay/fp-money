4ae5dde538e6a0c80e089bed6e8078ff8aa5ba74
brianvoe <bvoelker@fluidpay.com>
2025-10-16 14:12:10 -0500

    test - updated vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
-import { mergeConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-export default mergeConfig(viteConfig, defineConfig({
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom'
   }
-}))
+})
